3a55de653de3aa9565d714aad2514ad2537bca9e
onkar12002 <156268528+onkar12002@users.noreply.github.com>
2024-06-07 05:22:14 +0000

    asd

---
+++ production feature/pipeline.tf
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Terraform Init') {
+            steps {
+                sh 'cd /path/to/your/terraform/project && terraform init'
+            }
+        }
+        stage('Terraform approval') {
+            steps {
+                sh 'cd /path/to/your/terraform/project && terraform init'
+            }
+            
+        stage('Terraform Apply') {
+            steps {
+                sh 'cd /path/to/your/terraform/project && terraform apply -auto-approve'
+            }
+        }
+    }
+}
